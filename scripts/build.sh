93270d40c97c60717079ebba97dbd8b0c700b393
vba2000 <vba@mail.ru>
2018-07-26 18:58:11 +0300

    fix build

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-rm -rf dist
-./node_modules/.bin/tsc --downlevelIteration true -m commonjs -t es6 --outDir tmp
-./node_modules/.bin/browserify -o=./dist/index.js --entry=./tmp --no-bundle-external
-rm -rf tmp
