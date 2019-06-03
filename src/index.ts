734601d74766ce8e3798f4f4a406e73d2848d437
vba2000 <vba@mail.ru>
2019-06-03 11:48:50 +0300

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { find } from './utils';
 
 export * from './adapters';
 export * from './prepareTx';
+export { isValidAddress } from './prepareTx/fieldValidator';
 export * from './config';
 export * from './Signable';
 export * from './constants';
