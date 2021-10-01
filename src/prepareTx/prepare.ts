7dfc5c882cb3af4982188b68f9a5e1142bfd024b
okanishcheva <okanishcheva@wavesplatform.com>
2021-10-01 15:06:31 +0300

    types

--- src/prepareTx/prepare.ts
+++ src/prepareTx/prepare.ts
@@ -13,20 +13,20 @@ const normalizeAssetId = id => id === WAVES_ID ? '' : id;
 interface ICall { function: string, args?: Array<any> }
 
 export module prepare {
-    
+
     export module processors {
-        
+
         export function callFunc(callData?: ICall|null): ICall|null {
             if (!callData) {
                 return null;
             }
-            
+
             return {
                 function: callData && callData.function || '',
                 args: callData && callData.args || [],
             };
         }
-        
+
         export function payments(payments: Array<Money>) {
             return (payments || []).map(pay => {
                 return {
@@ -35,7 +35,7 @@ export module prepare {
                 }
             });
         }
-        
+
         export function paymentsToNode(payments: Array<Money>) {
             return (payments || []).map(pay => {
                 return {
@@ -44,22 +44,22 @@ export module prepare {
                 }
             });
         }
-        
+
         export function scriptProcessor(code: string): string | null {
             return !!(code || '').replace('base64:', '') ? code : null;
         }
-        
+
         export function assetPair(data: any) {
             return {
                 amountAsset: normalizeAssetId(data.amount.asset.id),
                 priceAsset: normalizeAssetId(data.price.asset.id)
             };
         }
-        
+
         export function signatureFromProof(proofs: any) {
             return proofs[0];
         }
-        
+
         export function toBigNumber(some: string | number | BigNumber | Money): BigNumber {
             switch (typeof some) {
                 case 'string':
@@ -73,11 +73,11 @@ export module prepare {
                     }
             }
         }
-        
+
         export function toNumberString(some: any) {
             return toBigNumber(some).toString();
         }
-        
+
         export function toSponsorshipFee(money: Money): BigNumber {
             const coins = money.getCoins();
             if (coins.eq(0)) {
@@ -87,55 +87,55 @@ export module prepare {
                 return coins;
             }
         }
-        
+
         export function moneyToAssetId(money: Money): string {
             return money.asset.id;
         }
-        
+
         export function moneyToNodeAssetId(money: Money): string {
             return idToNode(money.asset.id);
         }
-        
-        export function timestamp(time: string|number|Date) {
+
+        export function timestamp(time: string|number|Date): string | number {
             if (!(+time) && typeof time === 'string') {
                 return Date.parse(time);
             }
             return time && time instanceof Date ? time.getTime() : time;
         }
-        
+
         export function orString(data: any | string): string {
             return data || '';
         }
-        
+
         export function noProcess<T>(data: T): T {
             return data;
         }
-        
+
         //@ts-ignore
         export const recipient = networkByte => data => {
             return data.length <= 30 ? `alias:${networkByte}:${data}` : data;
         };
-        
+
         export function attachment(data: string | Array<number> | Uint8Array) {
             data = data || '';
             let value = data;
-            
+
             if (typeof data === 'string') {
                 value = stringToBytes(data);
             }
-            
+
             return base58Encode(Uint8Array.from(value as ArrayLike<number>));
         }
-        
+
         export function addValue(value: any) {
             return typeof value === 'function' ? value : () => value;
         }
-        
+
         //@ts-ignore
         export function expiration(date?) {
             return date || new Date().setDate(new Date().getDate() + 20);
         }
-        
+
         //@ts-ignore
         export function transfers(recipient, amount) {
             //@ts-ignore
@@ -144,17 +144,17 @@ export module prepare {
                 amount: amount(transfer.amount)
             }));
         }
-        
+
         //@ts-ignore
         export function quantity(data): BigNumber {
             return new BigNumber(data.quantity).mul(new BigNumber(10).pow(data.precision));
         }
-        
+
         //@ts-ignore
         export function base64(str): string {
             return (str || '').replace('base64:', '');
         }
-        
+
         //@ts-ignore
         export function toOrderPrice(order) {
             const assetPair = new AssetPair(order.amount.asset, order.price.asset);
@@ -162,7 +162,7 @@ export module prepare {
             return orderPrice.getMatcherCoins();
         }
     }
-    
+
     export function wrap(from: string | null, to: string, cb: any): IWrappedFunction {
         if (typeof cb != 'function') {
             //@ts-ignore
@@ -184,7 +184,7 @@ export module prepare {
         to: string;
         cb: Function;
     }
-    
+
     export function schema(...args: Array<IWrappedFunction | string>) {
         //@ts-ignore
         return (data) => args.map((item) => {
@@ -201,7 +201,7 @@ export module prepare {
                 return result;
             }, Object.create(null));
     }
-    
+
     //@ts-ignore
     export function signSchema(args: Array<{ name, field, processor, optional, type, optionalData }>) {
         //@ts-ignore
@@ -232,7 +232,7 @@ export module prepare {
                     } catch (e) {
                         errors.push(e);
                     }
-                    
+
                     return {
                         key: validateOptions.key,
                         value: null,
@@ -242,15 +242,15 @@ export module prepare {
                     result[key] = value;
                     return result;
                 }, Object.create(null));
-            
+
             if (errors.length) {
                 throw new Error(JSON.stringify(errors));
             }
-            
+
             return prepareData;
         };
     }
-    
+
     export function idToNode(id: string): string {
         return id === WAVES_ID ? '' : id;
     }
